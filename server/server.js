c1814e2cf251714eab30f7587d12d2d9affafee9
Gaganjot singh <gaganjot25@gmail.com>
2022-10-20 15:06:31 +0530

    passport.js project

--- server/server.js
+++ server/server.js
@@ -30,11 +30,14 @@ const app = express()
 const helmet = require('helmet')
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const cookieSession = require('cookie-session')
 
 const PORT = 3000
 const config = {
     CLIENT_ID: process.env.CLIENT_ID,
-    CLIENT_SECRET: process.env.CLIENT_SECRET
+    CLIENT_SECRET: process.env.CLIENT_SECRET,
+    COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+    COOKIE_KEY_2: process.env.COOKIE_KEY_2
 }
 const AUTH_OPTIONS = {
     callbackURL: '/auth/google/callback',
@@ -47,12 +50,26 @@ function verifyCallback(accessToken, refreshToken, profile, done) {
 }
 passport.use(new GoogleStrategy(AUTH_OPTIONS, verifyCallback))
 
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+})
+passport.deserializeUser((id, done) => {
+    done(null, id)
+})
+
 app.use(helmet())
+app.use(cookieSession({
+    name: 'session',
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2]
+
+}))
 app.use(passport.initialize())  
+app.use(passport.session())
 
 
 function checkLoggedIn(req,res,next) {
-    const isLoggedIn = true;
+    const isLoggedIn = req.user;
     if (!isLoggedIn) {
         return res.status(401).json({
             error: 'You must log in!'
@@ -66,7 +83,7 @@ app.get('/auth/google', passport.authenticate('google', {
 app.get('/auth/google/callback', passport.authenticate('google', {
     failureRedirect: '/failure',
     successRedirect: '/',
-    session: false
+    session: true
 }), (req,res) => {
     console.log('google called us back')
 })
